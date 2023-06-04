a2482a3185fe6082c969bbba061cfa02ba9a8dba
mariacuenca <marcuencac@outlook.com>
2023-06-04 19:53:12 -0300

    Se agrega reactivo login

--- front/MIRIFA.NET/src/app/pages/login/login.component.ts
+++ front/MIRIFA.NET/src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ServicioUsuarioService } from 'src/app/servicio-usuario.service';
+import { ServicioUsuarioService } from 'src/app/Servicios/servicio-usuario.service';
+import { FormBuilder } from '@angular/forms';
+import {Validators} from '@angular/forms'
+
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,33 @@ import { ServicioUsuarioService } from 'src/app/servicio-usuario.service';
 })
 export class LoginComponent {
 
-  constructor(private Miservicio:ServicioUsuarioService){}
+  export class LoginComponent {
+    invalido = "";
+    LoginForm = this.formBuilder.group({
+      user:['',[Validators.required, Validators.minLength(5)]],
+      password:['',[Validators.required,Validators.minLength(8)]],
 
-  add(Miservicio: string) {
-    this.MiServicio.muestraMensaje.push(Miservicio);
+    })
+
+
+  get user(){
+    return this.LoginForm.get("usuario")
   }
+  get password(){
+    return this.LoginForm.get("contraseña")
+  }
+ 
+
+  onEnviar(event: Event){
+    event.preventDefault;
+    if(this.LoginForm.valid){
+      alert("Enviar al servidor...")
+    }else{
+      this.LoginForm.markAllAsTouched()
+      this.invalido = "is-invalid border-danger"
+    }
+  }
+
+  constructor(private Miservicio:ServicioUsuarioService, private formBuilder:FormBuilder){}
+}
 }
